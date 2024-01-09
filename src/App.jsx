17f12ebff6a4734cdeb9c650236a633980af072c
alex-njuguna <njugunakinuthia013@gmail.com>
2024-01-09 23:12:53 +0300

    navbar boilerplate

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
-import { useState } from 'react'
 import './App.css'
+import Navbar from './components/Navbar'
 
 function App() {
 
   return (
     <>
-      <h1 className="alert alert-info">Hello world</h1>
+      <Navbar />
     </>
   )
 }
